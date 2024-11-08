c706ce0b7d0fa352fe2251c5330d0b722b099b53
Kirill <t2910690@ya.ru>
2024-11-08 18:42:44 +0700

    Добавлена супер-проверка на ввод неккорректной криптовалюты/валюты. В программе есть возможность вводить ту валюту, которой нет в предлагаемом списке популярных валют. Добавлен перевод в нижний регистр для введенной вручную криптовалюты.

--- converter.py
+++ converter.py
@@ -1,7 +1,9 @@
+# Приложение позволяет проверять как курс популярных валют, включенных в локальные словари,
+# так и непопулярных, название которых можно вводить вручную в представленном поле
+
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox as mb
-from wsgiref.validate import check_input
 
 import requests
 
@@ -16,16 +18,23 @@ def update_t_label(event):
     name = currencies[code]
     t_label.config(text=name)
 
+# Вывод сообщения
+def message_output(type_message, message):
+        if type_message == 'warning':
+            mb.showwarning("Предупреждение", f"Выберите {message}")
+        elif type_message == 'error':
+            mb.showerror('Ошибка', message)
+        else:
+            mb.showinfo('Справка', message)
+
 def crypto_exchange():
-    crypto_code = crypto_combobox.get() # Сохранить криптовалюту
+    crypto_code = crypto_combobox.get().lower() # Сохранить криптовалюту в нижнем регистре
     target_code = t_combobox.get() # Сохранить целевую валюту
-    # Проверка ввода
-    def check_input(message):
-        mb.showwarning("Предупреждение", f"Внимание! Выберите {message}")
+    # Проверить ввод пользователя
     if not crypto_code:
-        check_input('криптовалюту')
+        message_output('warning', 'криптовалюту')
     elif not target_code:
-        check_input('целевую валюту')
+        message_output('warning', 'целевую валюту')
     else:
         try:
             # Запрос курса конкретной криптовалюты в выбранной валюте
@@ -33,14 +42,18 @@ def crypto_exchange():
             crypto_response = requests.get(url, params=get_params(crypto_code, target_code))
             crypto_response.raise_for_status() # Проверить понимания запроса
             data = crypto_response.json()
-            exchange_rate = data[crypto_code][target_code.lower()]
 
-            # Сформировать сообщение с результатом
-            info = f"1 {crypto_code} торгуется на уровне {exchange_rate} {target_code}"
-            mb.showinfo("Курс обмена", info)
-
+            if crypto_response.json() == {}:
+                mb.showinfo('Предупреждение', 'Криптовалюта введена некорректно!')
+            elif crypto_response.json()[crypto_code] == {}:
+                mb.showinfo('Предупреждение', 'Целевая валюта введена некорректно!')
+            else:
+                exchange_rate = data[crypto_code][target_code.lower()]
+                # Сформировать сообщение с результатом
+                info = f"1 {crypto_code} торгуется на уровне {exchange_rate} {target_code}"
+                mb.showinfo("Курс обмена", info)
         except Exception as er:
-            mb.showerror("Предупреждение", f"{er}")
+            message_output('error', er)
 
 
 # Получение параметров для запроса
@@ -53,14 +66,21 @@ def get_params(ids, vs_currencies):
 
 # Словарь популярных криптовалют и их кириллических названий
 cryptocurrencies = {
-    'bitcoin': 'Биткойн',
-    'ethereum': 'Эфириум',
-    'ripple': 'Рипл (пульсация)',
-    'litecoin': 'Лайткойн (лёгкая монета)',
-    'cardano': 'Кардано'
+    'bitcoin': 'Биткойн (BTC)',
+    'ethereum': 'Эфириум (ETH)',
+    'dogecoin': 'Догикоин (DOGE)',
+    'algorand': 'Алгоранд (ALGO)',
+    'tron': 'Трон (TRX)',
+    'aave': 'Ааве (AAVE)',
+    'ripple': 'Рипл (XRP)',
+    'litecoin': 'Лайткойн (LTC)',
+    'cardano': 'Кардано (ADA)',
+    'tether': 'Тезер (USDT)',
+    'solana': 'Солана (SOL)',
+    'stacks': 'Стакс (STX)'
 }
 
-# Словарь кодов валют и их полных названий
+# Словарь популярных государственных валют и их полных названий
 currencies = {
     "USD": "Американский доллар",
     "EUR": "Евро",
