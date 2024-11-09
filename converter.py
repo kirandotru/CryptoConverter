65a0fd83b135fddfa6d85871bb42879f1e71fd39
Kirill <t2910690@ya.ru>
2024-11-09 16:15:36 +0700

    Добавлены комментарии к функциям, согласно Правилам Docstrings.

--- converter.py
+++ converter.py
@@ -7,19 +7,34 @@ import webbrowser
 
 #========================================================================
 # CONTROLLER / КОНТРОЛЛЕР — ПОЛУЧАТАЕТ И ПРЕОБРАЗУЕТ ЗАПРОСЫ ПОЛЬЗОВАТЕЛЯ
+
 def update_crypto_label(event):
+    """
+    Заполняет/обновляет метку для размещения названия криптовалюты.
+    :param event: Объект события
+    :return: None
+    """
     crypto_id = crypto_combobox.get() # Получить и сохранить выбранную криптовалюту
     crypto_name = cryptocurrencies[crypto_id]
     crypto_label.config(text=crypto_name)
 
-# Установка полного названия целевой валюты на метке
 def update_t_label(event):
+    """
+    Заполняет/обновляет метку для размещения названия целевой валюты.
+    :param event: Объект события
+    :return: None
+    """
     code = t_combobox.get()
     name = currencies[code]
     t_label.config(text=name)
 
-# Реализация вывода информационных сообщений в messagebox
 def message_output(type_message, message):
+    """
+    Выводит на экран сообщение messagebox, исходя из переданных параметров.
+    :param type_message: Тип сообщения
+    :param message: Содержание сообщения
+    :return: None
+    """
     if type_message == 'warning':
         mb.showwarning("Предупреждение", f"Выберите {message}")
     elif type_message == 'error':
@@ -27,33 +42,45 @@ def message_output(type_message, message):
     else:
         mb.showinfo('Справка', message)
 
-# Создание кнопки для получения справки
 def create_help(coin):
+    """
+    Создаёт кнопки для получения справки из стороннего ресурса.
+    :param coin: Наименование выбранной криптовалюты
+    :return: None
+    """
     for widget in help_frame.winfo_children():
         widget.destroy()
     help_button = Button(help_frame, text=f"Подробнее о монете '{coin.capitalize()}'", command=lambda: open_help(coin))
     help_button.pack()
 
-# Реализация открытия справки в браузере
 def open_help(cryptocurrency):
+    """
+    Реализует открытие справки из указанного ресурса в браузере по умолчанию.
+    :param cryptocurrency: Наименование выбранной криптовалюты
+    :return: None
+    """
     link = f"https://ru.wikipedia.org/wiki/{cryptocurrency}"
     webbrowser.open(link)
 
-# Реализация обмена криптовалют
 def crypto_exchange():
-    crypto_code = crypto_combobox.get().lower() # Сохранить криптовалюту в нижнем регистре
-    target_code = t_combobox.get() # Сохранить целевую валюту
-    # Проверить ввод пользователя
+    """
+    Реализует обмен криптовалют, ввёденных/выбранных пользователем.
+    Позволяет получать курс даже на те валюты, которые не указаны в локальном словаре.
+    :return: None
+    """
+    crypto_code = crypto_combobox.get().lower() # Сохранить криптовалюту в нижнем регистре.
+    target_code = t_combobox.get() # Сохранить целевую валюту.
+    # Проверить ввод пользователя.
     if not crypto_code:
         message_output('warning', 'криптовалюту')
     elif not target_code:
         message_output('warning', 'целевую валюту')
     else:
         try:
-            # Запрос курса конкретной криптовалюты в выбранной валюте
+            # Запрос курса конкретной криптовалюты в выбранной валюте.
             url = 'https://api.coingecko.com/api/v3/simple/price'
             crypto_response = requests.get(url, params=get_params(crypto_code, target_code))
-            crypto_response.raise_for_status() # Проверить понимания запроса
+            crypto_response.raise_for_status() # Проверить понимания запроса.
             data = crypto_response.json()
 
             if crypto_response.json() == {}:
@@ -62,17 +89,22 @@ def crypto_exchange():
                 mb.showinfo('Предупреждение', 'Целевая валюта введена некорректно!')
             else:
                 exchange_rate = data[crypto_code][target_code.lower()]
-                # Сформировать сообщение с результатом
+                # Сформировать сообщение с результатом.
                 info = f'1 {crypto_code} торгуется на уровне {exchange_rate} {target_code}'
                 result_label.config(text=f'Курс обмена:\n{info}')
-                # Создать кнопку помощи для выбранной криптовалюты
+                # Создать кнопку помощи для выбранной криптовалюты.
                 create_help(crypto_code)
 
         except Exception as er:
             message_output('error', er)
 
-# Получение параметров для запроса к API
 def get_params(ids, vs_currencies):
+    """
+    Формирует словарь параметров, необходимый для осуществления запроса к API.
+    :param ids: Название криптовалюты
+    :param vs_currencies: Название целевой валюты
+    :return: Параметры для запроса к API
+    """
     params = {
         'ids': ids,
         'vs_currencies': vs_currencies
@@ -82,7 +114,7 @@ def get_params(ids, vs_currencies):
 #=============================================
 # MODEL / МОДЕЛЬ — ОТВЕЧАЕТ ЗА ХРАНЕНИЕ ДАННЫХ
 
-# Словарь популярных криптовалют и их кириллических названий
+# Словарь популярных криптовалют и их кириллических названий.
 cryptocurrencies = {
     'bitcoin': 'Биткойн (BTC)',
     'ethereum': 'Эфириум (ETH)',
@@ -94,7 +126,7 @@ cryptocurrencies = {
     'tether': 'Тезер (USDT)',
 }
 
-# Словарь популярных государственных валют и их полных названий
+# Словарь популярных государственных валют и их полных названий.
 currencies = {
     "USD": "Американский доллар",
     "EUR": "Евро",
@@ -127,28 +159,28 @@ crypto_combobox = ttk.Combobox(values=list(cryptocurrencies.keys()))
 crypto_combobox.pack(padx=10, pady=5)
 crypto_combobox.bind("<<ComboboxSelected>>", update_crypto_label)
 
-# Блок комментария к выбранной криптовалюте
+# Блок комментария к выбранной криптовалюте.
 crypto_label = ttk.Label()
 crypto_label.pack(padx=10, pady=5)
 
-# Блок для целевой валюты
+# Блок для целевой валюты.
 Label(text="Целевая валюта:", font="Arial 10 bold").pack(padx=10, pady=5)
 t_combobox = ttk.Combobox(values=list(currencies.keys()))
 t_combobox.pack(padx=10, pady=5)
 t_combobox.bind("<<ComboboxSelected>>", update_t_label)
 
-# Блок комментария к выбранной целевой валюте
+# Блок комментария к выбранной целевой валюте.
 t_label = ttk.Label()
 t_label.pack(padx=10, pady=10)
 
 Button(text="Курс обмена", command=crypto_exchange).pack(padx=10,pady=15)
 
-# Блок результатов конвертации
+# Блок результатов конвертации.
 result_label = Label(width=100, height=4, background='#ffffff',
                      borderwidth=2, relief='groove', font="Arial 10")
 result_label.pack(padx=10, pady=5)
 
-# Фрейм для кнопки помощи
+# Фрейм для кнопки помощи.
 help_frame = Frame(window, width=300, height=100)
 help_frame.pack(pady=5)
 
